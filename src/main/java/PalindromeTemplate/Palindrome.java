f16ac835fb4a5b9d52c776262d539dc9c2c5ba69
dDyman1 <ddyman@student.touro.edu>
2021-01-25 16:52:24 -0500

    Template commit

---
+++ src/main/java/PalindromeTemplate/Palindrome.java
@@ -0,0 +1,15 @@
+package PalindromeTemplate;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Palindrome {
+    public static void main(String[] args) {
+
+    }
+
+    public static Set<String> findPalindromes(String word) {
+
+    }
+}
